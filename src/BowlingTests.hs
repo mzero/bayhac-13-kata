a22b8cfef1199a484c050fad25eaa1d4c78f59da
Mark Lentczner <markl@glyphic.com>
2013-05-17 22:30:56 -0700

    a property for dull (non-srike, non-spare) frames

--- src/BowlingTests.hs
+++ src/BowlingTests.hs
@@ -13,5 +13,22 @@ tests =
     [ testGroup "Simple Scores"
         [ testCase "zero" (score [] @?= 0)
         , testCase "some" (score [1,2,3,4] @?= 10)
+        , testProperty "gutters" prop_noTens
         ]
     ]
+
+data DullFrame = DullFrame Int Int
+    deriving (Show)
+instance Arbitrary DullFrame where
+    arbitrary = do
+        n <- choose (0,9)
+        a <- choose (0,n)
+        return $ DullFrame a (n - a)
+    shrink (DullFrame a b) =
+        [ DullFrame a' b' | a' <- [0..a], b' <- [0..b], a' + b' /= a + b ]
+
+prop_noTens :: [DullFrame] -> Bool
+prop_noTens dfs = score frameBowls == sum frameBowls
+  where
+    frameBowls = concatMap (\(DullFrame a b) -> [a,b]) dfs
+
